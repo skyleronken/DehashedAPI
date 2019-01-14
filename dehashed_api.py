671258b948376a8c51b87b371edba08436220505
Skyler Onken <2548705+skyleronken@users.noreply.github.com>
2019-01-14 08:21:39 -0500

    Updated file output and removal of duplicates

--- dehashed_api.py
+++ dehashed_api.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from time import sleep
 from math import ceil
 from json import loads
+from os import path, makedirs
 import requests
 import csv
 
@@ -14,6 +15,12 @@ LOGIN_URI = BASE_URI + "/login"
 SEARCH_URI = BASE_URI + "/search"
 QUERY_URI = SEARCH_URI + "?query="
 THROTTLE = 1
+ERROR_THRESHOLD = 5
+DUMP_NAME = 'full_dump.csv'
+UN_NAME = 'usernames.txt'
+PW_NAME = 'passwords.txt'
+RULER_NAME = 'ruler_format.txt'
+UNPW_NAME = 'username_password.csv'
 
 
 class LoginError(Exception):
@@ -26,7 +33,6 @@ class EntryFailureError(Exception):
 
 class DehashedAPI:
 
-    ERROR_THRESHOLD = 5
     session = None
     error_count = 0
 
@@ -46,21 +52,32 @@ class DehashedAPI:
         else:
             raise LoginError
 
-    def search(self, query, stdout=True, dump=False, unpw=False, un=False, pw=False):
+    def search(self, query, stdout=True, dump=False, unpw=False, un=False, pw=False, ruler=False, all=False):
 
         results = self.fetch_results(query)
         sorted_results = sorted(results, key=lambda k: k.get('username') or k.get('email'))
+
+        if all:
+            if not path.exists(all):
+                makedirs(all)
+            dump = path.join(all, DUMP_NAME)
+            unpw = path.join(all, UNPW_NAME)
+            un = path.join(all, UN_NAME)
+            pw = path.join(all, PW_NAME)
+            ruler = path.join(all, RULER_NAME)
 
         if stdout:
             self.print_results(sorted_results)
         if dump:
             self.dump_results(sorted_results, dump)
         if unpw:
-            self.dump_un_pw_list(sorted_results)
+            self.dump_un_pw_list(sorted_results, unpw)
         if un:
-            self.dump_un_list(sorted_results)
+            self.dump_un_list(sorted_results, un)
         if pw:
-            self.dump_pw_list(sorted_results)
+            self.dump_pw_list(sorted_results, pw)
+        if ruler:
+            self.dump_ruler(sorted_results, ruler)
 
     def check_results(self, query):
 
@@ -110,16 +127,68 @@ class DehashedAPI:
             raise EntryFailureError
 
     @classmethod
-    def dump_un_pw_list(cls, results):
-        pass
+    def dump_un_pw_list(cls, results, unpw_file):
+
+        ruler_pairs = list()
+
+        for result in results:
+            uname = result.get('username') or result.get('email')
+            pword = result['password']
+
+            if not uname or len(uname) < 1:
+                continue
+            if not pword or len(pword) < 1:
+                continue
+
+            ruler_pairs.append("{},{}".format(uname, pword))
+            unique_pairs = list(set(ruler_pairs))
+
+        with open(unpw_file, 'w') as file:
+            for pair in unique_pairs:
+                file.write("{}\n".format(pair))
 
     @classmethod
-    def dump_pw_list(cls, results):
-        pass
+    def dump_pw_list(cls, results, pw_file):
+
+        password_results = [res.get('password') for res in results]
+        unique_results = list(set(password_results))
+
+        with open(pw_file, 'w') as file:
+            for result in unique_results:
+                if result and len(result) > 0:
+                    file.write("{}\n".format(result))
 
     @classmethod
-    def dump_un_list(cls, results):
-        pass
+    def dump_un_list(cls, results, un_file):
+
+        username_results = [res.get('username') or res.get('email') for res in results]
+        unique_results = list(set(username_results))
+
+        with open(un_file, 'w') as file:
+            for result in unique_results:
+                if result and len(result) > 0:
+                    file.write("{}\n".format(result))
+
+    @classmethod
+    def dump_ruler(cls, results, ruler_file):
+
+        ruler_pairs = list()
+
+        for result in results:
+            uname = result['email'].split('@')[0]
+            pword = result['password']
+
+            if not uname or len(uname) < 1:
+                continue
+            if not pword or len(pword) < 1:
+                continue
+
+            ruler_pairs.append("{}:{}".format(uname, pword))
+            unique_pairs = list(set(ruler_pairs))
+
+        with open(ruler_file, 'w') as file:
+            for pair in unique_pairs:
+                file.write("{}\n".format(pair))
 
     @classmethod
     def dump_results(cls, results, dump_file):
@@ -145,6 +214,11 @@ if __name__ == "__main__":
     parser.add_argument('--username', dest='un', required=True, help='DeHashed Username')
     parser.add_argument('--password', dest='pw', required=True, help='DeHashed Password')
     parser.add_argument('--dump', dest='csv', default=False, help="Dump results to CSV file")
+    parser.add_argument('--ruler', dest='ruler', default=False, help="Dump {un}:{pw} format")
+    parser.add_argument('--pw', dest='pw', default=False, help="Dump list of passwords")
+    parser.add_argument('--un', dest='un', default=False, help="Dump list of usernames")
+    parser.add_argument('--unpw', dest='unpw', default=False, help="Dump lost of usernames/passwords")
+    parser.add_argument('--all', dest='all', default=False, help="Dump all formats")
     parser.add_argument('query', help='Query String')
 
     args = parser.parse_args()
@@ -163,6 +237,6 @@ if __name__ == "__main__":
         print("[+] Results: {}".format(num))
         input("[!] Press Enter to start fetching (Approximately {} minutes to complete)...".format(approx_time_min))
         print("[*] Querying: {}...".format(query_term))
-        api.search(query_term, dump=args.csv)
+        api.search(query_term, dump=args.csv, ruler=args.ruler, all=args.all, unpw=args.unpw, un=args.un, pw=args.pw)
     except LoginError as e:
         print("[-] Failed to authenticate")
